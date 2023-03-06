c182afe2048c8d27c222873b734093b2d22135c3
Rukhsana Yaqoob <bitf19m0362pucit.edu.pk>
2023-03-06 14:24:37 +0500

    scripts add in pacakage.json

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio1.png'
-import IMG6 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
+import IMG3 from '../../assets/portfolio3.png'
 
 const Portfolio = () => {
     return (
@@ -15,7 +15,7 @@ const Portfolio = () => {
                     <div className='portfolio__items-image'>
                         <img src={IMG1} alt='image 1'></img>
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Ecommerce Website in React</h3>
                     <div className='portfolio__item-cta'>
                         <a href='https://github.com/Wajeeha-Fatima/Front-End' className='btn' target='_blank'>Github</a>
                         <a href='https://www.readersclub.pk/' className='btn btn-primary' target='_blank'>Live Demo</a>
@@ -25,7 +25,7 @@ const Portfolio = () => {
                     <div className='portfolio__items-image'>
                         <img src={IMG2} alt='image 2'></img>
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Quran Application</h3>
                     <div className='portfolio__item-cta'>
                         <a href='https://github.com/Wajeeha-Fatima/Quran-Majeed' className='btn' target='_blank'>Github</a>
                     </div>
@@ -33,12 +33,12 @@ const Portfolio = () => {
 
                 <article className='portfolio__items'>
                     <div className='portfolio__items-image'>
-                        <img src={IMG6} alt='image 6'></img>
+                        <img src={IMG3} alt='image 3'></img>
                     </div>
-                    <h3>This is a portfolio item title</h3>
+                    <h3>Shopping Cart in JS</h3>
                     <div className='portfolio__item-cta'>
-                        <a href='https://github.com' className='btn' target='_blank'>Github</a>
-                        <a href='https://dribbble.com' className='btn btn-primary' target='_blank'>Live Demo</a>
+                        <a href='https://github.com/Wajeeha-Fatima/Shopping-Cart-JS' className='btn' target='_blank'>Github</a>
+                        {/* <a href='https://dribbble.com' className='btn btn-primary' target='_blank'>Live Demo</a> */}
                     </div>
                 </article>
             </div>
